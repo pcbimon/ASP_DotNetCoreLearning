5dde17259f15f3e5887ae21d489beb1bf99d6941
Patipat C <pcbimon@gmail.com>
2019-08-14 11:27:48 +0700

    Custom route url

--- ASP_DotNetCoreLearning/Controllers/MoviesController.cs
+++ ASP_DotNetCoreLearning/Controllers/MoviesController.cs
@@ -31,7 +31,9 @@ namespace ASP_DotNetCoreLearning.Models
             }
             return Content(string.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
         }
-        public ActionResult ByReleaseDate(int year,int month)
+        //Custom Route url
+        [Route("movies/released/{year}/{month:regex(\\d{{2}}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year,int month)
         {
             return Content(year + "/" + month);
         }
